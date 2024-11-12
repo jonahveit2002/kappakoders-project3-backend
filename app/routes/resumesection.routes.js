822fa3c42b4858e400d212e60ce2af5b80e6ef81
KyleD0711 <kyle.denney@eagles.oc.edu>
2024-11-12 09:35:07 -0600

    Fixed reumsesection router

--- app/routes/resumesection.routes.js
+++ app/routes/resumesection.routes.js
@@ -1,15 +1,31 @@
 module.exports = (app) => {
-  const resumeSection = require("../controllers/resume.controller.js");
+  const resumeSection = require("../controllers/resumesection.controller.js");
   const { authenticate } = require("../authorization/authorization.js");
   var router = require("express").Router();
 
-  router.get("/resume/:id", [authenticate], resume.getForId);
+  router.get(
+    "/resume/:resumeId/resumeSection",
+    [authenticate],
+    resumeSection.getAllForResumeId
+  );
 
-  router.post("/resume/", [authenticate], resume.create);
+  router.post(
+    "/resume/:resumeId/resumeSection",
+    [authenticate],
+    resumeSection.create
+  );
 
-  router.put("/resume/:id", [authenticate], resume.update);
+  router.put(
+    "/resume/:resumeId/resumeSection",
+    [authenticate],
+    resumeSection.update
+  );
 
-  router.delete("/resume/:id", [authenticate], resume.delete);
+  router.delete(
+    "/resume/:resumeId/resumeSection",
+    [authenticate],
+    resumeSection.delete
+  );
 
   app.use("/resume-t1/student", router);
 };
