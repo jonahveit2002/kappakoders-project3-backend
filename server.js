04efec59402fce1eb75cf4d7182c722ef64a3939
KyleD0711 <kyle.denney@eagles.oc.edu>
2024-11-12 09:22:34 -0600

    Added template route and controller

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ require("./app/routes/experience.routes.js")(app);
 require("./app/routes/project.routes.js")(app);
 require("./app/routes/skill.routes.js")(app);
 require("./app/routes/resume.routes.js")(app);
+require("./app/routes/template.routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3011;
