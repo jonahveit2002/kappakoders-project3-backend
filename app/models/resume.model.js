5e1f380633f999f82ea921b131fb6a07d45d2409
Frostbit912 <97480950+JonathanBooker912@users.noreply.github.com>
2024-11-18 17:28:32 -0600

    Added get all resumes route and name attribute to resume table

--- app/models/resume.model.js
+++ app/models/resume.model.js
@@ -5,6 +5,10 @@ module.exports = (sequelize, Sequelize) => {
       autoIncrement: true,
       primaryKey: true,
     },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
     metadata: {
       type: Sequelize.JSON,
       allowNull: false,
