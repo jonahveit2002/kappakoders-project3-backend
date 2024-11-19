85c75dfc7f164bb0997a8843a28cd3f7e0d71519
Yorsh <george.hartman@eagles.oc.edu>
2024-11-18 19:12:09 -0600

    Forced user role change in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 const db = require("./app/models");
 
-db.sequelize.sync({ alter: true });
+db.sequelize.sync({ force: true });
 
 var corsOptions = {
   origin: "http://localhost:8081",
