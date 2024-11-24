2a49af0d515e8fa26afbb2de0beb907fae369ee5
Yorsh <george.hartman@eagles.oc.edu>
2024-11-23 21:00:15 -0600

    SQL removed

--- app/controllers/userrole.controller.js
+++ app/controllers/userrole.controller.js
@@ -32,37 +32,6 @@ const getAllForUser = async (req, res) => {
     raw: true,
   });
 
-
-  
-  try {
-    const userRoles = await db.sequelize.query(
-      `SELECT
-        users.id AS userId,
-        users.fName AS firstName,
-        users.lName AS lastName,
-        users.email AS userEmail,
-        users.phoneNum AS phoneNum,
-        roles.type as roleId
-        FROM 
-          users
-        JOIN
-          userroles ON users.id = userroles.userId
-        JOIN 
-          roles ON userroles.userId = roles.id
-        WHERE
-          users.id = :userId;`,
-          {
-            replacements: {userId},
-            type: db.Sequelize.QueryTypes.SELECT
-          }
-    );
-    res.status(200).json(userRoles);
-    res.send(userRoles);
-  }  catch (error) {
-    console.error("Error fetching user roles:", error);
-    res.status(500).json({ message: "Error fetching user roles" });
-  }
-
 };
 
 module.exports = { getAllForUser };
